90d065d6d1f8779e98ead317f0fe54f4f571c3d2
nojuhyeon <njh2720@gmail.com>
2025-06-02 04:20:23 +0000

    update log

--- bemon_db_update.py
+++ bemon_db_update.py
@@ -3,7 +3,9 @@ from data_setting.MYSQL_CRUD import MySQLDatabase
 from datetime import datetime
 
 db = MySQLDatabase()
-
-database_updater = BillingDatabaseUpdater()
-database_updater.update_database()
-print(f"CSP Billing Data Updated: {datetime.now()}")
+try:
+    database_updater = BillingDatabaseUpdater()
+    database_updater.update_database()
+    print(f"CSP Billing data updated successfully at: {datetime.now()}")
+except Exception as e:
+    print(f"Failed to update CSP Billing data at: {datetime.now()} - Error: {e}")
