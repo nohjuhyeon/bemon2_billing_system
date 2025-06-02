b8991874eb58654ca5cf01100dd9cfde279b1413
nojuhyeon <njh2720@gmail.com>
2025-06-02 04:17:00 +0000

    log 출력력

--- bemon_db_update.py
+++ bemon_db_update.py
@@ -1,7 +1,9 @@
 from data_setting.mysql_user_data_setting import BillingDatabaseUpdater
 from data_setting.MYSQL_CRUD import MySQLDatabase
+from datetime import datetime
 
 db = MySQLDatabase()
 
 database_updater = BillingDatabaseUpdater()
 database_updater.update_database()
+print(f"CSP Billing Data Updated: {datetime.now()}")
