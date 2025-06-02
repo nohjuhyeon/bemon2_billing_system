ab5572b704067f731d63ef7fc68f36f8d1dbcebb
nojuhyeon <njh2720@gmail.com>
2025-06-02 01:00:53 +0000

    test

--- bemon_db_init_setting.py
+++ bemon_db_init_setting.py
@@ -4,7 +4,7 @@ from data_setting.MYSQL_CRUD import MySQLDatabase
 db = MySQLDatabase()
 
 database_updater = BillingDatabaseUpdater()
-database_updater.create_database_from_sql()
+# database_updater.create_database_from_sql()
 
-database_updater.user_list_insert()
+# database_updater.user_list_insert()
 database_updater.update_database()
